78eeb3b45901514de1d1f0b402ed358e4f2a7a88
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2023-09-23 13:17:43 +0200

    setup elysia

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const app = new Elysia().get("/", () => "Hello world from Elysia").listen(3000);
+console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
