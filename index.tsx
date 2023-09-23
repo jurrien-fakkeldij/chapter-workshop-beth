78a6992156b764a71fa002b72b708cc4fa02ef78
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2023-09-23 13:27:56 +0200

    added htmx

--- index.tsx
+++ index.tsx
@@ -7,9 +7,14 @@ const app = new Elysia()
     .get("/", ({ html }) => html(
         <BaseHtml>
             <body>
-                <h1>Hello World from Elysia Dom!</h1> I am a html document with a component.
+                <button hx-post="/clicked" hx-swap="outerHTML">
+                    Click Me
+                </button>
             </body>
         </BaseHtml>
+    ))
+    .post("/clicked", () => (
+        <div>Now I am a div and not a button anymore returned from the server</div>
     ))
     .listen(3000);
 console.log(`Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
@@ -20,6 +25,7 @@ const BaseHtml = ({ children }: elements.Children) => `
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<script src="https://unpkg.com/htmx.org@1.9.5" integrity="sha384-xcuj3WpfgjlKF+FXhSQFQ0ZNr39ln+hwjN3npfM9VBnUskLolQAcN80McRIVOPuO" crossorigin="anonymous"></script>
 <title>Chapter Workshop</title>
 </head>
 ${children}
